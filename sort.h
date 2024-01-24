823109b69d55fc320c001d8c0cd6c5bd26e01921
Dahrey <138259597+Dahrey@users.noreply.github.com>
2024-01-24 22:50:27 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void cocktail_sort_list(int *array, size_t size);
 
 #endif
